ebaef9c1903296381515bd2961b6190a3a63b3ac
ngocxxu <ngocquach43@gmail.com>
2025-10-28 23:02:24 +0700

    feat: update

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL = "http://localhost:3002/api/v1";
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:3002/api/v1";
 
 export const STORAGE_KEYS = {
   ACCESS_TOKEN: "accessToken",
